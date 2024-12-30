6e2a7163e547811e4d4db673e29a31b7256ad902
[Muji] <[zxc215236qq@gmail.com]>
2024-12-30 23:57:12 +0800

    feat: collision detection

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 # 初始化 pygame
 pygame.init()
@@ -40,11 +41,16 @@ playerSpeed = 5
 # Enemy類
 class Enemy():
     def __init__(self, img, x, y, speed):
-        self.imgEnemy = img
-        self.enemySize = 32
-        self.enemyX = x
-        self.enemyY = y
-        self.enemySpeed = speed
+        self.health = 3
+        self.img = img
+        self.size = 32
+        self.x = x
+        self.y = y
+        self.speed = speed
+    # 受擊
+    def Hurt(self):
+        self.health -= 1
+    
 
 numbersOfEnemies = 5
 enemies = []
@@ -53,34 +59,50 @@ for i in range(numbersOfEnemies):
 
 # 生成Enemy
 def ShowEnemy():
-    global enemyX, enemyY, enemySpeed
-    for i in enemies:
-        screen.blit(i.imgEnemy, (i.enemyX, i.enemyY))
-        i.enemyX += i.enemySpeed
-        if i.enemyX > gameWidth + 2*i.enemySize or i.enemyX < 0 - 2*i.enemySize:
-            i.enemySpeed *= -1
-            # todo 
-            # 刪除自身
+    global x, y, speed
+    for e in enemies:
+        screen.blit(e.img, (e.x, e.y))
+        e.x += e.speed
+        # 出界判斷
+        if e.x > gameWidth + 2*e.size or e.x < 0 - 2*e.size:
+            e.speed *= -1
+            # todo 刪除自身
     
 # Bullet類
 class Bullet():
     def __init__(self):
         self.img = imgPlayerBullet01
-        self.x = playerX
-        self.y = playerY + 10
+        self.x = playerX + 8
+        self.y = playerY - 10
         self.speed = 10
-bullets = [] #保存現有子彈
+    #碰撞檢測
+    def Collision(self):
+        for e in enemies:
+            if(distance(self.x, self.y, e.x, e.y) < 15):
+                if self in bullets:  # 確保子彈仍然存在於列表中
+                    bullets.remove(self)
+                e.Hurt()  # 減少敵人生命值
+
+ #保存現有子彈
+bullets = []
 
 # 生成子彈
 def showBullets():
-    for i in bullets:
-        screen.blit(i.img, (i.x, i.y))
-        i.y -= i.speed
-        if i.y < -30:
-            bullets.remove(i)
+    bullets_to_remove = []  # 暫存需要移除的子彈
+    for b in bullets:
+        screen.blit(b.img, (b.x, b.y))
+        b.Collision()  # 碰撞檢測
+        b.y -= b.speed
+        if b.y < -30:
+            bullets_to_remove.append(b)
+    # 移除需要刪除的子彈
+    for bullet in bullets_to_remove:
+        if bullet in bullets:  # 確保子彈仍然存在於列表中
+            bullets.remove(bullet)
+
 
 # 設定射擊間隔和追蹤上次射擊時間
-shootCooldown = 300  # 300毫秒（0.3秒）
+shootCooldown = 200
 lastShootTime = 0  # 初始化為0
 
 # Player事件處理
@@ -103,6 +125,11 @@ def PlayerEvents():
             bullets.append(Bullet())
             lastShootTime = currentTime  # 更新上次射擊時間
 
+# 距離判定(歐氏距離)
+def distance(x1, y1, x2, y2):
+    a = x1 - x2
+    b = y1 - y2
+    return math.sqrt(a*a + b*b) #開根號
 
 # 繪製右側UI框
 def DrawUI():
